7c806b8d7f72ebd04025a241a5cabe6bbbca4105
pyadminrus <zh.vitaliy92@yandex.ru>
2023-08-27 23:34:22 +0300

    Проверка подписки на канал

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,9 +1,9 @@
-from aiogram import types  # Типы пользователя
+from aiogram import types
 from aiogram.dispatcher import FSMContext  # Состояния пользователя
+
 from keyboards.user_keyboards import greeting_keyboards  # Клавиатуры поста приветствия
 from messages.user_messages import greeting_post  # Пояснение для пользователя FAG
-
-from system.dispatcher import dp  # Подключение к боту и диспетчеру пользователя
+from system.dispatcher import dp, bot, CHANNEL_ID  # Подключение к боту и диспетчеру пользователя
 
 
 @dp.message_handler(commands=['start'])
@@ -17,6 +17,23 @@ async def greeting(message: types.Message, state: FSMContext):
                         parse_mode=types.ParseMode.HTML)
 
 
+@dp.callback_query_handler(text='get_password')
+async def get_password(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    chat_id = callback_query.message.chat.id
+
+    # Проверка подписки на канал
+    user = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
+    if user.status == "member" or user.status == "administrator":
+        # Пользователь подписан, отправляем файл с паролем
+        # Замените 'password.txt' на путь к файлу с паролем
+        with open('setting/password.txt', 'rb') as password_file:
+            await bot.send_document(chat_id, password_file)
+    else:
+        # Пользователь не подписан, отправляем сообщение с просьбой подписаться
+        await bot.send_message(chat_id, "Пожалуйста, подпишитесь на канал @TG_SMM2 и попробуйте снова.")
+
+
 def greeting_handler():
-    """Регистрируем handlers для калькулятора"""
+    """Регистрируем handlers для бота"""
     dp.register_message_handler(greeting)  # Обработчик команды /start, он же пост приветствия
--- system/dispatcher.py
+++ system/dispatcher.py
@@ -8,6 +8,7 @@ from aiogram.dispatcher import Dispatcher
 config = configparser.ConfigParser(empty_lines_in_values=False, allow_no_value=True)
 config.read("setting/config.ini")  # Чтение файла
 bot_token = config.get('BOT_TOKEN', 'BOT_TOKEN')  # Получение токена
+CHANNEL_ID = config.get('CHANNEL_ID', 'CHANNEL_ID')
 
 bot = Bot(token=bot_token, parse_mode="HTML")
 storage = MemoryStorage()  # Хранилище
