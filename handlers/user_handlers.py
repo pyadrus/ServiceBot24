7553b602038ac546aebbbcbbccad7249e52ca485
pyadminrus <zh.vitaliy92@yandex.ru>
2023-08-28 00:08:42 +0300

    Запись ID пользователя в базу данных

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from aiogram import types
 from aiogram.dispatcher import FSMContext  # Состояния пользователя
 
@@ -25,13 +27,42 @@ async def get_password(callback_query: types.CallbackQuery):
     # Проверка подписки на канал
     user = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
     if user.status == "member" or user.status == "administrator":
-        # Пользователь подписан, отправляем файл с паролем
-        # Замените 'password.txt' на путь к файлу с паролем
-        with open('setting/password.txt', 'rb') as password_file:
-            await bot.send_document(chat_id, password_file)
+        # Проверка наличия ID в базе данных
+        cursor.execute('SELECT id FROM users WHERE id = ?', (user_id,))
+        result = cursor.fetchone()
+
+        if result:
+            # Пользователь подписан и имеет ID в базе данных, отправляем файл с паролем
+            # Замените 'password.txt' на путь к файлу с паролем
+            with open('setting/password.txt', 'rb') as password_file:
+                await bot.send_document(chat_id, password_file)
+        else:
+            # Пользователь не имеет ID в базе данных
+            await bot.send_message(chat_id, "Вы должны быть зарегистрированы и подписаны на канал @TG_SMM2.")
     else:
         # Пользователь не подписан, отправляем сообщение с просьбой подписаться
         await bot.send_message(chat_id, "Пожалуйста, подпишитесь на канал @TG_SMM2 и попробуйте снова.")
+
+
+# Инициализация базы данных SQLite
+conn = sqlite3.connect('setting/user_data.db')
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY)')
+
+
+# Обработчик команды /id
+@dp.message_handler(commands=['id'])
+async def process_id_command(message: types.Message):
+    try:
+        user_id = int(message.text.split()[1])
+
+        # Запись ID в базу данных
+        cursor.execute('INSERT INTO users (id) VALUES (?)', (user_id,))
+        conn.commit()
+
+        await message.reply(f"ID {user_id} успешно записан в базу данных.")
+    except (IndexError, ValueError):
+        await message.reply("Используйте команду /id followed by ваш ID.")
 
 
 def greeting_handler():
