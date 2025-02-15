b615802f383d8b35e3a2428d9de064fd25b5017f
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-02-15 09:10:58 +0300

    Рефакторинг кода

--- handlers/payments/products_goods_services.py
+++ handlers/payments/products_goods_services.py
@@ -1,7 +1,7 @@
 password_TelegramMaster = 150.00  # Сумма товара пароль TelegramMaster 2.0
 password_TelegramMaster_Commentator = 150.00  # Сумма товара пароль TelegramMaster_Commentator
 
-TelegramMaster = 1.00  # Стоимость товара TelegramMaster 2.0
+TelegramMaster = 1200.00  # Стоимость товара TelegramMaster 2.0
 TelegramMaster_Commentator = 1000.00  # Сумма товара "TelegramMaster_Commentator"
 
 payment_installation = 500.00  # Сумма товара "Установка ПО"
